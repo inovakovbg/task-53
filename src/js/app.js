08a6236121662cc87b9d3f151a0df1f0956099d2
Ivan Novakov <inovakov.bg@gmail.com>
2023-01-18 11:31:18 +0200

    Add
    add atribute

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,14 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const products = document.querySelector('.product');
+  console.log(products)
+
+  const Span = products.querySelector('.price');
+  const price = Span.textContent;
+
+  console.log(price)
+  products.setAttribute('data-price', price);
 
 
 });
